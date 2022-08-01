f657d64b817dd94e6c5435a52a90828c0ffc2d90
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-08-01 23:55:44 +0530

    Create C++.cpp

---
+++ Find inorder predecessor for the given key in a BST/Iterative Version/C++.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+ 
+// Data structure to store a BST node
+struct Node
+{
+    int data;
+    Node* left = nullptr, *right = nullptr;
+ 
+    Node() {}
+    Node(int data): data(data) {}
+};
+ 
+// Recursive function to insert a key into a BST
+Node* insert(Node* root, int key)
+{
+    // if the root is null, create a new node and return it
+    if (root == nullptr) {
+        return new Node(key);
+    }
+ 
+    // if the given key is less than the root node, recur for the left subtree
+    if (key < root->data) {
+        root->left = insert(root->left, key);
+    }
+    // if the given key is more than the root node, recur for the right subtree
+    else {
+        root->right = insert(root->right, key);
+    }
+ 
+    return root;
+}
+ 
+// Helper function to find the maximum value node in a given BST
+Node* findMaximum(Node* root)
+{
+    while (root->right) {
+        root = root->right;
+    }
+ 
+    return root;
+}
+ 
+// Iterative function to find inorder predecessor for a given key in a BST
+Node* findPredecessor(Node* root, int key)
+{
+    // base case
+    if (root == nullptr) {
+        return nullptr;
+    }
+ 
+    Node* prec = nullptr;
+ 
+    while (1)
+    {
+        // if the given key is less than the root node, visit the left subtree
+        if (key < root->data) {
+            root = root->left;
+        }
+ 
+        // if the given key is more than the root node, visit the right subtree
+        else if (key > root->data)
+        {
+            // update predecessor to the current node before visiting
+            // right subtree
+            prec = root;
+            root = root->right;
+        }
+ 
+        // if a node with the desired value is found, the predecessor is the maximum
+        // value node in its left subtree (if any)
+        else {
+            if (root->left) {
+                prec = findMaximum(root->left);
+            }
+            break;
+        }
+ 
+        // if the key doesn't exist in the binary tree, return previous greater node
+        if (!root) {
+            return prec;
+        }
+    }
+ 
+    // return predecessor, if any
+    return prec;
+}
+ 
+int main()
+{
+    int keys[] = { 15, 10, 20, 8, 12, 16, 25 };
+ 
+    /* Construct the following tree
+               15
+             /    \
+            /      \
+           10       20
+          / \      /  \
+         /   \    /    \
+        8    12  16    25
+    */
+ 
+    Node* root = nullptr;
+    for (int key: keys) {
+        root = insert(root, key);
+    }
+ 
+    // find inorder predecessor for each key
+    for (int key: keys)
+    {
+        Node* prec = findPredecessor(root, key);
+ 
+        if (prec != nullptr) {
+            cout << "The predecessor of node " << key << " is " << prec->data << endl;
+        }
+        else {
+            cout << "The predecessor doesn't exist for " << key << endl;
+        }
+    }
+ 
+    return 0;
+}
